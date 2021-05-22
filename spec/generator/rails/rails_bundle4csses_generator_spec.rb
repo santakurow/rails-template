893e1781403cec9551e5aeebe1d60505df7be88a
santakurow <newtakuproj0730@gmail.com>
2021-05-22 20:28:23 +0900

    テンプレートファイルの作成

--- spec/generator/rails/rails_bundle4csses_generator_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Rails::Bundle4csses", type: :generator do
-
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
--- template.rb
+++ template.rb
@@ -1,4 +1,4 @@
-class Rails::Bundle4cssGenerator < Rails::Generators::NamedBase
+if yes?("Would you like to setup webpack bundle for assets(css)?")
   gsub_file "app/views/layouts/application.html.erb", "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", "<%= stylesheet_pack_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>"
 
   run "mkdir -p app/javascript/stylesheets && touch app/javascript/stylesheets/application.scss"
