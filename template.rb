b38a82d1a2d4d18851677e8a373ed7410550d491
santakurow <newtakuproj0730@gmail.com>
2021-07-10 08:53:48 +0900

    applicationテンプレートをhaml(slim)に変換したあとにtemplate.rbが実行できないバグを修正

--- template.rb
+++ template.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
-def stylesheet_tag_for(tag_name)
-  "<%= stylesheet_#{tag_name}_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' >"
+def application_layout_path(ext)
+  File.expand_path("#{Rails.root}/app/views/layouts/application.html.#{ext}", __FILE__)
 end
 
+file_ext = %w(erb haml slim).select do |ext|
+  file_path = application_layout_path(ext)
+  FileTest.exist?(file_path)
+end.first
+
 if yes?('デフォルトでcss(sass)をwebpackで管理するように設定しますか？ (y/n)')
-  gsub_file 'app/views/layouts/application.html.erb',
-            stylesheet_tag_for('link'),
-            stylesheet_tag_for('pack')
+  gsub_file application_layout_path(file_ext), 
+            "stylesheet_link_tag", "stylesheet_pack_tag"
 
   run 'mkdir -p app/javascript/stylesheets && touch app/javascript/stylesheets/application.scss'
 
-  inject_into_file 'app/javascript/packs/application.js', "import '../stylesheets/application'\n",
-                   before: /^Rails.start()/
+  append_to_file 'app/javascript/packs/application.js', "import '../stylesheets/application'\n"
 end
